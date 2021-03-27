12c10654ad687dec0e617a99eeeca9e7fef19b6b
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2021-03-27 13:05:09 +0600

    changed apis

--- BicycleMap/src/Screens/MapScreen.js
+++ BicycleMap/src/Screens/MapScreen.js
@@ -5,7 +5,7 @@ import { Text, StyleSheet } from "react-native"
 const MapScreen = () => {
     return(
     <Text style={{alignSelf:"center" , }}>
-        HARRY POTTER
+        HARRY POTTERS
 </Text>
     )
 }
