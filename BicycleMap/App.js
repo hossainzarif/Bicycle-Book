bc9ade0de003f35950985cea9219dda8305e55d3
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2021-03-27 10:54:11 +0600

    Created Screens and navigation

--- BicycleMap/App.js
+++ BicycleMap/App.js
@@ -1,10 +1,10 @@
 
 
 import React from 'react';
-import { Text, StyleSheet,StatusBar } from "react-native"
+import { Text, StyleSheet, StatusBar } from "react-native"
 import { createStackNavigator } from "@react-navigation/stack"
 import { NavigationContainer } from "@react-navigation/native"
-
+import Map from "./src/Screens/Map"
 
 
 
@@ -17,10 +17,11 @@ const MapStack = createStackNavigator()
 
 
 function App() {
-    return
-    (
+    return(
         <NavigationContainer>
-
+            <MapStack.Navigator>
+                <MapStack.Screen name = "Map" component= {Map} />
+            </MapStack.Navigator>
         </NavigationContainer>
     )
 }
