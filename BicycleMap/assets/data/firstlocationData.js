cb5e10c3e5a6426b0a543a53d05292e6c4a4bd79
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2021-03-28 17:49:19 +0600

    New assets added

---
+++ BicycleMap/assets/data/firstlocationData.js
@@ -0,0 +1,50 @@
+const firstlocationData = [
+    {
+      id: 'cycle-1',
+      title: 'Elecetro cycle bx29',
+      image: require('../images/cycle-1.jpg'),
+      Battery:80,
+      available: true,
+      price: 10,
+      weight:20,
+      maxSpeed:120,
+    },
+    {
+      id: 'cycle-2',
+      title: 'Elecetro cycle cx42',
+
+      image: require('../images/cycle-2.jpg'),
+      Battery:90,
+      available: true,
+      price: 16,
+      weight:20,
+      maxSpeed:120,
+    },
+    {
+      id: 'cycle-3',
+      title: 'Elecetro cycle rx23',
+
+      image: require('../images/cycle-3.jpg'),
+      Battery:70,
+      available: true,
+      price: 15,
+      weight:20,
+      maxSpeed:120,
+    },
+    {
+      id: 'cycle-4',
+      title: 'Elecetro cycle ax29',
+
+      image: require('../images/cycle-4.jpg'),
+      Battery:80,
+      available: false,
+      price: 20,
+      weight:20,
+      maxSpeed:120,
+    },
+
+
+
+  ]
+
+    export default firstlocationData
