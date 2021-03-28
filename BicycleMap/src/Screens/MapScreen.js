a9ccead18defaf5097c5df2c900eff07b065631f
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2021-03-28 17:50:32 +0600

    MapPage Modified

--- BicycleMap/src/Screens/MapScreen.js
+++ BicycleMap/src/Screens/MapScreen.js
@@ -5,7 +5,7 @@ import CycleCard from "../Components/CycleCard"
 const MapScreen = () => {
     return(
 
-        <View style={{flex:1,alignItems:"center"}}>
+        <View style={{flex:1,alignItems:"center",justifyContent:"center"}}>
             <CycleCard/>
         </View>
 
