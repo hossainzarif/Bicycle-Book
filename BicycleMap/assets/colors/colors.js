203d288a05ef7ba47a5a1432b522d725b6c55d19
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2021-03-29 14:31:14 +0600

    added color

---
+++ BicycleMap/assets/colors/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+    gray: '#D1D3D2',
+    darkGray: '#676767',
+    orange: '#db5e40',
+    black: '#0C0D0E',
+    white: '#FBFCFE',
+    buttoncolor:"#4465f9",
+    green: "#1ad91a"
+  };
+  
+  export default colors;
